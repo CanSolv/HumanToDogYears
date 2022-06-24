6313cc23a1cda29e36a4a59a70a15815779c9deb
Can Yucel Timur <can.timur@solvoyo.com>
2022-06-24 09:22:02 +0300

    Files created

---
+++ dogYears.js
@@ -0,0 +1,7 @@
+var myAge = 17
+let earlyYears = 2
+earlyYears *= 10.5
+let laterYears = myAge - 2
+laterYears *= 4
+myAge_inDogYears = earlyYears + laterYears
+console.log('My age in dog years is '+ myAge_inDogYears )
